de3634b65355880697080d9822c447a50636b3d4
c-usher <usher.cody@outlook.com>
2021-08-24 14:49:27 -0600

    about page is done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,7 +4,25 @@ import "./about.css";
 export default function About() {
   return (
     <div className="about-page">
-      <div>ABOUT PAGE</div>
+      <h1 className="name">Cody Usher</h1>
+      <div className="about">
+        <p>
+          I am a full-stack developer living in Colorado. I have a passion for
+          developing applications that will help people in their daily life.
+        </p>
+        <p>
+          Graduating General Assembly has motivated me to follow my dream and
+          become a Software Engineer full-time. I have experience with AWS as
+          well as MERN stack applications. I am also familiar with PostgresSQL
+          and other database management tools. I am a quick learner and a hard
+          worker.
+        </p>
+        <p>
+          I like to stay busy and practice my craft by always having an
+          application or game I am working on. For an example of my work, please
+          take a look at my portfolio section.
+        </p>
+      </div>
     </div>
   );
 }
